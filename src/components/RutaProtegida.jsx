c19f5ce0d6b6a7467f70024a3d91654350fba190
Paola <murillojimenezpaola1@gmail.com>
2025-05-17 10:06:03 -0500

    se soluciona error de autorizacion de errores

--- src/components/RutaProtegida.jsx
+++ src/components/RutaProtegida.jsx
@@ -1,10 +1,25 @@
-import { Navigate } from 'react-router-dom';
+/*import { Navigate } from 'react-router-dom';
 
 function RutaProtegida({ children, rolRequerido }) {
   const token = localStorage.getItem("token");
   const rol = localStorage.getItem("rol");
 
   if (!token || (rol !== rolRequerido && rol !== "admin")) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
+
+export default RutaProtegida;*/
+
+import { Navigate } from 'react-router-dom';
+
+function RutaProtegida({ children, rolesPermitidos }) {
+  const token = localStorage.getItem("token");
+  const rol = localStorage.getItem("rol");
+
+  if (!token || (rolesPermitidos && !rolesPermitidos.includes(rol))) {
     return <Navigate to="/" />;
   }
 
